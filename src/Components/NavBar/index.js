187da1f549c156f6b80bc204d2de4d4c84282ca6
jmash <jaredmashcraft@gmail.com>
2020-02-22 13:41:11 -0500

    :construction: Add profile pic to navbar

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import profilePicUrl from '../../assets/img/jaredprofile.jpg';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-                <li>
-                    <Link to="/portfolio">Portfolio</Link>
-                </li>
-            </ul>
+            <img src={ profilePicUrl } />
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/portfolio">Portfolio</Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
