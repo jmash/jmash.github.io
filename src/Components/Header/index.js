3e47b981f008642ce38bb5ac0e84259ca4aad06d
jmash <jaredmashcraft@gmail.com>
2020-02-28 20:08:43 -0500

    Switches router links to LinkContainers

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import profilePicUrl from '../../assets/img/jaredprofile.jpg';
-import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavItem from 'react-bootstrap/NavItem';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
     return (
         <div>
             <Navbar>
-                <img className={ styles.profilePic } src={ profilePicUrl } alt="Jared M Ashcraft"/>
-                <div>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">Contact</Link>
-                        </li>
-                        <li>
-                            <Link to="/portfolio">Portfolio</Link>
-                        </li>
-                    </ul>
-                </div>
+                <Navbar.Brand>
+                    <img className={ styles.profilePic } src={ profilePicUrl } alt="Jared M Ashcraft"/>
+                </Navbar.Brand>
+                <Nav>
+                    <LinkContainer to="/">
+                        <NavItem>Home</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/contact">
+                        <NavItem>Contact</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/portfolio">
+                        <NavItem>Portfolio</NavItem>
+                    </LinkContainer>
+                </Nav>
             </Navbar>
         </div>
     );
