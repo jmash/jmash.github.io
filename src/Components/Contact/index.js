b5817c97622f288650e6dddee6f0b5dee8f00493
jmash <jaredmashcraft@gmail.com>
2020-03-01 01:02:18 -0500

    :lipstick: Style contact page

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+// import styles from './Contact.module.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 
 const Contact = () => {
     return (
-        <div>
-            <span>Contact</span>
-        </div>
+        <Jumbotron className="w-75 mx-auto">
+            <Container>
+                <span>I'm Jared!</span>
+            </Container>
+        </Jumbotron>
     );
 };
 
