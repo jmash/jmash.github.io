836383f9452ba75ef915711c9617ef7044d7a82c
jmash <jaredmashcraft@gmail.com>
2020-03-02 15:47:15 -0500

    :construction: Adding fontawesome for contact

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 // import styles from './Contact.module.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => {
     return (
         <Jumbotron className="w-75 mx-auto">
             <Container>
-                <span>I'm Jared!</span>
+                <h1 className="display-6">Jared M Ashcraft</h1>
+                <hr />
+                <FontAwesomeIcon icon={ faEnvelope } />
             </Container>
         </Jumbotron>
     );
