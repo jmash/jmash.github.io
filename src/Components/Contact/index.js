e92fa6d36aa06d2eca937f9da6937bb951c83f60
jmash <jaredmashcraft@gmail.com>
2020-03-03 14:52:47 -0500

    :construction: Adds obfuscated email to contact

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Obfuscate from 'react-obfuscate';
 // import styles from './Contact.module.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
@@ -11,8 +13,10 @@ const Contact = () => {
             <Container>
                 <h1 className="display-6">Jared M Ashcraft</h1>
                 <hr />
-                <FontAwesomeIcon className="fa-2x" icon={ faEnvelope } />
-                
+                <Row>
+                    <FontAwesomeIcon className="fa-2x" icon={ faEnvelope } />
+                    <Obfuscate style={{ display:"inline-block" }} email="jaredmashcraft@gmail.com" />
+                </Row>
             </Container>
         </Jumbotron>
     );
