56d36c42d390a70bbcbacde6c29df34b1d2731bd
jmash <jaredmashcraft@gmail.com>
2020-03-03 15:29:47 -0500

    :construction: Adds obf phone # to contact

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -5,7 +5,7 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => {
     return (
@@ -16,6 +16,10 @@ const Contact = () => {
                 <Row>
                     <FontAwesomeIcon className="fa-2x" icon={ faEnvelope } />
                     <Obfuscate style={{ display:"inline-block" }} email="jaredmashcraft@gmail.com" />
+                </Row>
+                <Row>
+                    <FontAwesomeIcon className="fa-2x" icon= { faPhone } />
+                    <Obfuscate style={{ display:"inline-block" }} tel="407-632-2207" />
                 </Row>
             </Container>
         </Jumbotron>
