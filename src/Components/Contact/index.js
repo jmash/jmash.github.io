98e826e3eee631d093d01eb700db88ec9b275fd5
jmash <jaredmashcraft@gmail.com>
2020-03-04 14:17:36 -0500

    :lipstick: updates styles for contact

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Obfuscate from 'react-obfuscate';
-// import styles from './Contact.module.css';
+import cx from 'classnames';
+import bsStyles from '../../assets/global-styles/bootstrap.module.css';
+import faStyles from '../../assets/global-styles/fontawesome.module.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -12,14 +14,26 @@ const Contact = () => {
         <Jumbotron className="w-75 mx-auto mt-4">
             <Container>
                 <h1 className="display-6">Jared M Ashcraft</h1>
+                <h3 className="pb-2">Web Developer</h3>
                 <hr />
                 <Row>
-                    <FontAwesomeIcon className="fa-2x" icon={ faEnvelope } />
-                    <Obfuscate style={{ display:"inline-block" }} email="jaredmashcraft@gmail.com" />
+                    <FontAwesomeIcon className={cx(faStyles['fa-2x'])} icon={ faEnvelope } />
+                    <Obfuscate 
+                        className={
+                            cx(bsStyles['ml-2'], bsStyles['mt-1'], bsStyles['font-weight-bold'])
+                        } 
+                        style={{ display:"inline-block" }} 
+                        email="jaredmashcraft@gmail.com" 
+                    />
                 </Row>
                 <Row>
                     <FontAwesomeIcon className="fa-2x" icon= { faPhone } />
-                    <Obfuscate style={{ display:"inline-block" }} tel="407-632-2207" />
+                    <Obfuscate className={
+                            cx(bsStyles['ml-2'], bsStyles['mt-1'], bsStyles['font-weight-bold'])
+                        } 
+                        style={{ display:"inline-block" }}  
+                        tel="407-632-2207" 
+                    />
                 </Row>
             </Container>
         </Jumbotron>
