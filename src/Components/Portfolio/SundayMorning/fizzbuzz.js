fa23eb8b7e1974b4057d6454daa9ddac6535c73d
jmash <jaredmashcraft@gmail.com>
2020-03-11 12:17:18 -0400

    :bug: fixed fizzbuzz generation function

--- src/Components/Portfolio/SundayMorning/fizzbuzz.js
+++ src/Components/Portfolio/SundayMorning/fizzbuzz.js
@@ -1,12 +1,15 @@
 export default class FizzBuzz {
-    constructor(limit=15) {
-        this.limit = limit;
+    constructor() {
         this.fbResult = "";
     }
 
-    generateFB() {
+    generateFB(limit=15) {
+        console.log(limit);
+        if(limit > 9999) limit = 9999;
+        if(limit <= 0) limit = 1;
+
         this.fbResult = "";
-        for(let i = 1; i <= this.limit; i++) {
+        for(let i = 1; i <= limit; i++) {
             if(i % 3 === 0 && i % 5 === 0) this.fbResult += `${i} FizzBuzz\n`;
             else if(i % 3 === 0) this.fbResult += `${i} Fizz\n`;
             else if(i % 5 === 0) this.fbResult += `${i} Buzz\n`;
