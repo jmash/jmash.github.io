02a79137b7410262c3e7b1c28f212ab6baec7a0c
Jared M Ashcraft <jaredmashcraft@gmail.com>
2020-06-10 22:01:27 -0400

    :recycle: Made Letter Analyzer its own component

---
+++ src/Components/Portfolio/SundayMorning/LetterAnalyzer/index.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react';
+import Chart from 'chart.js';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import letterAnalyzerStyles from './LetterAnalyzer.module.css';
+import cx from 'classnames';
+import { interpret } from 'xstate';
+import { letterAnalyzerMachine } from './letterAnalyzerMachine';
+import { Subject } from 'rxjs';
+
+const letterAnalyzerSubj = new Subject();
+
+class LetterAnalyzerLogic {
+    static createGraph = (cxt, xs, ys) => {
+        const laOptions = {
+            type: 'bar',
+            data: {
+                labels: xs,
+                datasets: [{
+                    label: 'Letter Analysis',
+                    data: ys,
+                    backgroundColor: '#112233',
+                    borderColor: '#112233'
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        }
+
+        return new Chart(cxt, laOptions);
+    }
+
+    static dumpGraph(graph) {
+        graph.data.labels.pop();
+        graph.data.datasets.forEach((dataset) => {
+            dataset.data.pop();
+        });
+        graph.update();
+    }
+
+    static updateGraph(xs, ys, graph) {
+        graph.data.labels = xs;
+        graph.data.datasets[0].data.push(ys);
+        graph.data.datasets[0].label = "Letter Analysis";
+        graph.data.datasets[0].data = ys;
+        graph.update();
+    }
+}
+
+export default class LetterAnalyzer extends Component {
+    constructor() {
+        super();
+
+        this.letterAnalyzerService = interpret(letterAnalyzerMachine).onTransition(laCurrent =>
+            this.setState({ laCurrent })
+        );
+
+        this.canvasRef = React.createRef();
+
+        this.state = {
+            laCharMax: 10000,
+            laCharCount: 0,
+            laUpdateCount: 0,
+            laInput: "",
+            laCurrent: letterAnalyzerMachine.initialState
+        }
+    }
+
+    componentDidMount() {
+        let letterAnalyzerGraph = LetterAnalyzerLogic.createGraph(this.canvasRef.current, [], []);
+
+        this.letterAnalyzerService.start();
+
+        letterAnalyzerSubj.subscribe({
+            next: () => {
+                let letterBreakdown = {};
+                let characters = this.state.laInput.split('');
+                characters = characters.map(c => c.toLowerCase());
+                characters = characters.sort();
+                
+                for(let char in characters) {
+                    if(letterBreakdown[characters[char]]) {
+                        if(characters[char].toUpperCase() !== characters[char].toLowerCase())
+                            letterBreakdown[characters[char]]++;
+                    } else {
+                        if(characters[char].toUpperCase() !== characters[char].toLowerCase())
+                            letterBreakdown[characters[char]] = 1;
+                    }
+                }
+                let xs = [];
+                let ys = [];
+                for(let l in letterBreakdown) {
+                    xs.push(l);
+                    ys.push(letterBreakdown[l]);
+                }
+
+                LetterAnalyzerLogic.dumpGraph(letterAnalyzerGraph);
+                LetterAnalyzerLogic.updateGraph(xs, ys, letterAnalyzerGraph);                
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.letterAnalyzerService.stop();
+    }
+
+    handleLAInputChange = (e) => {
+        this.setState({
+            laCharCount: e.target.value.length,
+            laInput: e.target.value
+        }, () => {
+            this.letterAnalyzerService.send({ type: 'CHANGE', length: this.state.laCharCount });
+            console.log(this.state.laCharCount);
+        });
+    }
+
+    render() {
+        return (
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Title>Letter Analyzer</Card.Title>
+                        <Card.Subtitle>Enter some text and get a breakdown of how many of each letter was used</Card.Subtitle>
+                        <Card.Body>
+                            <Form.Group>
+                                <Form.Control className={cx(letterAnalyzerStyles['no-resize'])} as="textarea" rows={5} onChange={this.handleLAInputChange} />
+                                    <Form.Text>Limit of 10000 characters - {this.state.laCharCount}/{this.state.laCharMax} characters left</Form.Text>
+                                <Button 
+                                    disabled={this.state.laCurrent.matches('error') ||
+                                                this.state.laCurrent.matches('start') } 
+                                    onClick={() => letterAnalyzerSubj.next() }>
+                                        Generate Letter Analysis
+                                </Button>
+                            </Form.Group>
+                            <canvas ref={this.canvasRef} id="letterAnalyzerChart" width="400" height="400"/>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        )
+    }
+}
--- src/Components/Portfolio/SundayMorning/index.js
+++ src/Components/Portfolio/SundayMorning/index.js
@@ -2,97 +2,12 @@ import React, { Component } from 'react';
 import FizzBuzz from './FizzBuzz';
 import Palindrome from './Palindrome';
 import RandomQuote from './RandomQuote';
-import LetterAnalyzer from './letterAnalyzer.js';
+import LetterAnalyzer from './LetterAnalyzer';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import sundaymorningStyles from './SundayMorning.module.css';
-import cx from 'classnames';
-import { interpret } from 'xstate';
-import { letterAnalyzerMachine } from './letterAnalyzerMachine';
-import { Subject } from 'rxjs';
-
-
-
-const letterAnalyzerSubj = new Subject();
-
 
 class SundayMorning extends Component {
-    constructor() {
-        super();
-
-        this.letterAnalyzerService = interpret(letterAnalyzerMachine).onTransition(laCurrent =>
-            this.setState({ laCurrent })
-        );
-
-        this.canvasRef = React.createRef();
-
-        this.state = {
-            
-            laCharMax: 10000,
-            laCharCount: 0,
-            laUpdateCount: 0,
-            laInput: "",
-            
-            laCurrent: letterAnalyzerMachine.initialState
-        }
-    }
-
-    componentDidMount() { 
-        let letterAnalyzerGraph = LetterAnalyzer.createGraph(this.canvasRef.current, [], []);
-
-        letterAnalyzerSubj.subscribe({
-            next: () => {
-                let letterBreakdown = {};
-                let characters = this.state.laInput.split('');
-                characters = characters.map(c => c.toLowerCase());
-                characters = characters.sort();
-                
-                for(let char in characters) {
-                    if(letterBreakdown[characters[char]]) {
-                        if(characters[char].toUpperCase() !== characters[char].toLowerCase())
-                            letterBreakdown[characters[char]]++;
-                    } else {
-                        if(characters[char].toUpperCase() !== characters[char].toLowerCase())
-                            letterBreakdown[characters[char]] = 1;
-                    }
-                }
-                let xs = [];
-                let ys = [];
-                for(let l in letterBreakdown) {
-                    xs.push(l);
-                    ys.push(letterBreakdown[l]);
-                }
-
-                console.log(xs);
-                console.log(ys);
-
-                LetterAnalyzer.dumpGraph(letterAnalyzerGraph);
-                LetterAnalyzer.updateGraph(xs, ys, letterAnalyzerGraph);                
-            }
-        });
-    }
-
-    componentWillUnmount() {
-        
-        this.letterAnalyzerService.stop();
-    }
-
-    
-
-    handleLAInputChange = (e) => {
-        this.setState({
-            laCharCount: e.target.value.length,
-            laInput: e.target.value
-        }, () => {
-            this.letterAnalyzerService.send({ type: 'CHANGE', length: this.state.laCharCount });
-            console.log(this.state.laCharCount);
-        });
-    }
-
     render() {
         return (
             <Container>
@@ -105,27 +20,7 @@ class SundayMorning extends Component {
                 <FizzBuzz />
                 <Palindrome />
                 <RandomQuote />
-                <Row>
-                    <Col>
-                        <Card>
-                            <Card.Title>Letter Analyzer</Card.Title>
-                            <Card.Subtitle>Enter some text and get a breakdown of how many of each letter was used</Card.Subtitle>
-                            <Card.Body>
-                                <Form.Group>
-                                    <Form.Control className={cx(sundaymorningStyles['no-resize'])} as="textarea" rows={5} onChange={this.handleLAInputChange} />
-                                        <Form.Text>Limit of 10000 characters - {this.state.laCharCount}/{this.state.laCharMax} characters left</Form.Text>
-                                    <Button 
-                                        disabled={this.state.laCurrent.matches('error') ||
-                                                  this.state.laCurrent.matches('start') } 
-                                        onClick={() => letterAnalyzerSubj.next() }>
-                                            Generate Letter Analysis
-                                    </Button>
-                                </Form.Group>
-                                <canvas ref={this.canvasRef} id="letterAnalyzerChart" width="400" height="400"/>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
+                <LetterAnalyzer />
             </Container>
         );
     }
