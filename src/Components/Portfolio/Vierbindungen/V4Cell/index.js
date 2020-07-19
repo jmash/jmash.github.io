0b426fe5e57ecdbf5afffe059a6a3ea6afdd476c
Jared M Ashcraft <jaredmashcraft@gmail.com>
2020-07-19 16:07:40 -0400

    :construction: Fixes V4 cell styling

--- src/Components/Portfolio/Vierbindungen/V4Cell/index.js
+++ src/Components/Portfolio/Vierbindungen/V4Cell/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import cx from 'classnames';
+import v4Styles from './V4Cell.module.css';
 
-export const K4Cell = (props) => {
+const K4Cell = (props) => {
     return(
-        <div class="cell mx-0 px-0">
-        <svg class="coutout" xmlns="http://www.w3.org/2000/svg" version="1.1">
-            <defs>
-            <mask id="hollow">
-                <rect width="48" height="48" fill="white" />         
-                <circle r="20" cx="24" cy="24" fill="black" />
-            </mask>
-            </defs>
-            <rect width="48" height="48" fill="grey" mask="url(#hollow)"/>
-        </svg>
+        <div className={cx(v4Styles['cell'], "mx-0", "px-0")}>
+            <svg class="cutout" xmlns="http://www.w3.org/2000/svg" version="1.1">
+                <defs>
+                    <mask id="hollow">
+                        <rect width="48" height="48" fill="white" />         
+                        <circle r="20" cx="24" cy="24" fill="black" />
+                    </mask>
+                </defs>
+                <rect width="48" height="48" fill="grey" mask="url(#hollow)"/>
+            </svg>
         </div>
     );
 }
+
+export default K4Cell;
