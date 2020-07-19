4af2a35fcb1ff6c7da50218e62443ef4d9db4265
Jared M Ashcraft <jaredmashcraft@gmail.com>
2020-07-19 16:04:52 -0400

    :lipstick: bootstrap import unnecessary

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import profilePicUrl from '../../assets/img/jaredprofile.jpg';
 import headerStyles from './Header.module.css';
-import bsStyles from '../../assets/global-styles/bootstrap.module.css';
 import faStyles from '../../assets/global-styles/fontawesome.module.css';
 import { LinkContainer } from 'react-router-bootstrap';
 import cx from 'classnames';
@@ -13,28 +12,28 @@ import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-ic
 
 const Header = () => {
     return (
-        <Navbar className={ cx(headerStyles.navBar, bsStyles['px-0'])} bg="light" variant="light">
+        <Navbar className={ cx(headerStyles.navBar, 'px-0')} bg="light" variant="light">
             <Navbar.Brand>
-                <img className={ cx(headerStyles.profilePic, bsStyles['ml-3']) } src={ profilePicUrl } alt="Jared M Ashcraft"/>
+                <img className={ cx(headerStyles.profilePic, 'ml-3') } src={ profilePicUrl } alt="Jared M Ashcraft"/>
             </Navbar.Brand>
             <Navbar.Collapse>
                 <NavItem>
                     <a href="https://twitter.com/jaredmashcraft">
-                        <FontAwesomeIcon className={ cx(bsStyles['mx-2'], faStyles['fa-3x']) } icon={ faTwitter } />
+                        <FontAwesomeIcon className={ cx('mx-2', faStyles['fa-3x']) } icon={ faTwitter } />
                     </a>
                 </NavItem>
                 <NavItem>
                     <a href="https://github.com/jmash">
-                        <FontAwesomeIcon className={ cx(bsStyles['mx-2'], faStyles['fa-3x']) } icon={ faGithub } />
+                        <FontAwesomeIcon className={ cx('mx-2', faStyles['fa-3x']) } icon={ faGithub } />
                     </a>
                 </NavItem>
                 <NavItem>
                     <a href="https://www.linkedin.com/in/jared-ashcraft-2985b939/">
-                        <FontAwesomeIcon className={ cx(bsStyles['mx-2'], faStyles['fa-3x']) } icon={ faLinkedin } />
+                        <FontAwesomeIcon className={ cx('mx-2', faStyles['fa-3x']) } icon={ faLinkedin } />
                     </a>
                 </NavItem>
             </Navbar.Collapse>
-            <Nav className={ cx(headerStyles.navTabs, bsStyles['container-fluid']) }>
+            <Nav className={ cx(headerStyles['navTabs'], 'container-fluid', 'mr-3') }>
                 <LinkContainer to="/about">
                     <NavItem className="ml-auto">About</NavItem>
                 </LinkContainer>
