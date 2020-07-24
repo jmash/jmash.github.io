eff31836c89358c05ca4e14c06ee0c58c33af07a
Jared M Ashcraft <jaredmashcraft@gmail.com>
2020-07-24 15:03:31 -0400

    :construction: start on Vierbindungen comp

--- src/Components/Portfolio/Vierbindungen/index.js
+++ src/Components/Portfolio/Vierbindungen/index.js
@@ -1,11 +1,27 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import vierStyles from './Vierbindungen.module.css';
+import cx from 'classnames';
+import { interpret, useMachine } from 'xstate';
 import V4Cell from './V4Cell';
+import { V4Machine } from './vierbindungenMachine.js';
 
 
 export const Vierbindungen = (props) => {
     const [gridWidth, setGridWidth] = useState(7);
     const [gridHeight, setGridHeight] = useState(6);
-    const [gameState, setGameState] = useState(0);
+    const [current, send] = useMachine(V4Machine);
+
+    useEffect(() => {
+        v4MachineService.start();
+        const v4MachineService = interpret(V4Machine).onTransition(v4Current => {
+            setGameCurrent(v4Current);
+        });
+
+        return function cleanup() {
+            v4MachineService.stop();
+        }
+    });
     
     let rows = [];
     
@@ -16,12 +32,23 @@ export const Vierbindungen = (props) => {
         }
         rows.push(row);
     }
-    let grid = <div class="d-flex justify-content-center v4container">{rows.map(row => <div class="flex-row">{row}</div>)}</div>
+    let grid =  <div className={cx("d-flex", "justify-content-center", vierStyles['v4container'])}>
+                    {rows.map((row, i) => <div key={i} className="flex-row">{row}</div>)}
+                </div>
     
+    function handleStartClick() {
+        v4MachineService.send({ type: 'START' });
+        console.log(v4MachineService._state);
+    }
+
     return(
-        <div class="">
-            <p>Vierbindungen</p>
+        <div className={cx("d-flex", "justify-content-center", "flex-column", "align-items-center")}>
             {grid}
+            <div className={cx("mt-4")}>
+                <span className={cx("mr-4", vierStyles['v4playerIndicOff'])}>Player 1</span>
+                <Button onClick={handleStartClick}>Start Game</Button>
+                <span className={cx("ml-4", vierStyles['v4playerIndicOff'])}>Player 2</span>
+            </div>
         </div>
     );
 }
