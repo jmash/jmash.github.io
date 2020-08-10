9049806d331875f8925be62a66853ee5c5f52bc4
Jared M Ashcraft <jaredmashcraft@gmail.com>
2020-08-10 01:33:42 -0400

    :poop: adds temporary coloring

--- src/Components/Portfolio/Vierbindungen/V4Cell/index.js
+++ src/Components/Portfolio/Vierbindungen/V4Cell/index.js
@@ -4,7 +4,7 @@ import v4Styles from './V4Cell.module.css';
 
 const V4Cell = (props) => {
     return(
-        <div className={cx(v4Styles['cell'], "mx-0", "px-0")}>
+        <div onClick={props.onClick} className={cx(v4Styles['cell'], "mx-0", "px-0")}>
             <svg width="48" height="48" xmlns="http://www.w3.org/2000/svg" version="1.1">
                 <defs>
                     <mask id="hollow">
@@ -13,6 +13,7 @@ const V4Cell = (props) => {
                     </mask>
                 </defs>
                 <rect width="48" height="48" fill="grey" mask="url(#hollow)" z-index="1"/>
+                <circle r="20" cx="24" cy="24" fill={props.color} />
             </svg>
         </div>
         
