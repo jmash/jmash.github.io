d5b02eed918fdb4ff5a4991b2dc032690f3b2f9a
Jared M Ashcraft <jaredmashcraft@gmail.com>
2021-06-29 15:51:44 -0400

    :construction: Clock component built

--- src/Components/Portfolio/Clock/index.js
+++ src/Components/Portfolio/Clock/index.js
@@ -1,14 +1,85 @@
 import React from 'react';
 import Sketch from 'react-p5';
 
+
+
 const Clock = () => {
+    const maxWidth = 400;
+    const maxHeight = 400;
+    let date = new Date();
+    let currentSecond = date.getSeconds();
+    let currentMinute = date.getMinutes();
+    let currentHour = date.getHours();
+
+    const updateTime = () => {
+        date = new Date();
+        currentSecond = date.getSeconds();
+        currentMinute = date.getMinutes();
+        currentHour = date.getHours();
+    }
+
+    const translateToCenter = (p5) => {
+        p5.translate(maxWidth/2, maxHeight/2);
+    }
+
+    const drawClockBorder = (p5) => {
+        p5.stroke('black');
+        p5.circle(0, 0, (maxWidth/2)+2);
+    }
+
+    const drawCenterKnob = (p5) => {
+        p5.strokeWeight(6);
+        p5.point(0, 0);
+    }
+
+    const drawClockGraduations = (p5) => {
+        for(let i = 0; i < 360; i++) {
+            p5.strokeCap(p5.PROJECT);
+            p5.strokeWeight(1);
+            if(i % 30 === 0) {
+              p5.strokeWeight(3);
+            }
+            if(i % 6 === 0) { 
+              p5.line(95*p5.cos(i), 95*p5.sin(i), 100*p5.cos(i), 100*p5.sin(i));
+            }
+        }
+    }
+
+    const drawSecondsHand = (p5) => {
+        p5.strokeWeight(1);
+        p5.line(0, 0, 90*p5.cos(currentSecond*6-90), 90*p5.sin(currentSecond*6-90));
+    }
+
+    const drawMinutesHand = (p5) => {
+        p5.strokeWeight(3);
+        p5.line(0, 0, 90*p5.cos((currentMinute+(currentSecond/60))*6-90), 90*p5.sin((currentMinute+(currentSecond/60))*6-90));
+    }
+
+    const drawHoursHand = (p5) => {
+        p5.strokeWeight(4);
+        p5.line(0, 0, 60*p5.cos((currentHour+(currentMinute/60)+(currentSecond/3600))*30-90), 60*p5.sin((currentHour+(currentMinute/60)+(currentSecond/3600))*30-90));
+    }
+
+    const updateClockFrame = (p5) => {
+        updateTime();
+        translateToCenter(p5);
+        drawClockBorder(p5);
+        drawCenterKnob(p5);
+        drawClockGraduations(p5);
+        drawSecondsHand(p5);
+        drawMinutesHand(p5);
+        drawHoursHand(p5);
+
+    }
+
     const setup = (p5, canvasParentRef) => {
         p5.createCanvas(500, 500).parent(canvasParentRef);
+        p5.angleMode(p5.DEGREES);
     }
 
     const draw = (p5) => {
-        p5.background(0);
-        p5.ellipse(50, 50, 70, 70);
+        p5.background(220);
+        updateClockFrame(p5);
     }
 
     return <Sketch setup={setup} draw={draw} />;
