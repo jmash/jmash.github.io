36a154a409383a2aff770e1a4abc7088a2cd6d9c
Jared M Ashcraft <jaredmashcraft@gmail.com>
2021-06-29 19:56:39 -0400

    :construction: restructure portfolio navbar

--- src/Components/Portfolio/PortfolioHeader/index.js
+++ src/Components/Portfolio/PortfolioHeader/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
+import cx from 'classnames';
+import portfolioHeaderStyles from './PortfolioHeader.module.css';
 
 const PortfolioHeader = () => {
     return (
-        <Nav variant="pills">
-            <LinkContainer to="/portfolio/sundaymorning">
-                <Nav.Item>Sunday Morning</Nav.Item>
-            </LinkContainer>
-            <LinkContainer to="/portfolio/vierbindungen">
-                <Nav.Item>Vierbindungen</Nav.Item>
-            </LinkContainer>
-            <LinkContainer to="/portfolio/clock">
-                <Nav.Item>Clock</Nav.Item>
-            </LinkContainer>
-        </Nav>
+        <Navbar variant='pills'>
+            <Container>
+                <LinkContainer className={cx(portfolioHeaderStyles['']) to="/portfolio/sundaymorning">
+                    <Nav.Item>Sunday Morning</Nav.Item>
+                </LinkContainer>
+                <LinkContainer to="/portfolio/vierbindungen">
+                    <Nav.Item>Vierbindungen</Nav.Item>
+                </LinkContainer>
+                <LinkContainer to="/portfolio/clock">
+                    <Nav.Item>Clock</Nav.Item>
+                </LinkContainer>
+            </Container>
+        </Navbar>
     );
 };
 
