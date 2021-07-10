6b4f9353f8edaadd00a81ae7c99370caa0a61588
Jared M Ashcraft <jaredmashcraft@gmail.com>
2021-07-09 23:38:55 -0400

    :construction: Remove unnecessary imports

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,16 +4,10 @@ import headerStyles from './Header.module.css';
 import faStyles from '../../assets/global-styles/fontawesome.module.css';
 import { LinkContainer } from 'react-router-bootstrap';
 import cx from 'classnames';
-import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Button from 'react-bootstrap/Button';
-import { NavDropdown } from 'react-bootstrap';
-import { FormControl } from 'react-bootstrap';
-import { Form } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { NavbarBrand } from 'react-bootstrap';
 
 const Header = () => {
     return (
@@ -23,7 +17,7 @@ const Header = () => {
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className={cx('mr-auto')}>
+                <Nav className={cx('mr-auto', 'px-md-4')}>
                     <Nav.Link href="https://twitter.com/jaredmashcraft">
                             <FontAwesomeIcon className={ cx('mx-2', faStyles['fa-2x']) } icon={ faTwitter } />
                     </Nav.Link>
--- src/Components/Portfolio/PortfolioHeader/index.js
+++ src/Components/Portfolio/PortfolioHeader/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import cx from 'classnames';
 import portfolioHeaderStyles from './PortfolioHeader.module.css';
