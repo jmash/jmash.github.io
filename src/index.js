2e96491720d32beacda19a62ef552430b5459280
Jared M Ashcraft <jaredmashcraft@gmail.com>
2021-07-17 14:06:06 -0400

    :construction: import highlightjs css style

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'highlight.js/styles/a11y-dark.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
