9e911791e01d9c29c5a4b26a66f082779e75c41a
Jared M Ashcraft <jaredmashcraft@gmail.com>
2021-07-25 15:52:28 -0400

    :recycle: Refactor SundayMorning comp

--- src/Components/Portfolio/SundayMorning/index.js
+++ src/Components/Portfolio/SundayMorning/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import ProgramCard from './ProgramCard';
+import Container from 'react-bootstrap/Container';
 import FizzBuzz from './FizzBuzz';
 import Palindrome from './Palindrome';
 import RandomQuote from './RandomQuote';
 import LetterAnalyzer from './LetterAnalyzer';
 import TicTacToe from './TicTacToe';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
@@ -18,11 +19,11 @@ class SundayMorning extends Component {
                         <h2>Easy like a smooth cup o' joe on a breezy Sunday morning <span role="img" aria-label="coffee">☕</span></h2>
                     </Col>
                 </Row>
-                <FizzBuzz />
-                {/* <Palindrome />
-                <RandomQuote />
-                <LetterAnalyzer />
-                <TicTacToe /> */}
+                <ProgramCard program={<FizzBuzz />} programTitle={'FizzBuzz'} programSubtitle={'The classic Fizzbuzz'} />
+                <ProgramCard program={<Palindrome />} programTitle={'Palindrome Checker'} programSubtitle={'Checks if a word or sentence is a palindrome!'} />
+                <ProgramCard program={<RandomQuote />} programTitle={'Random Quote Machine'} programSubtitle={'Press the button, get a random quote!'} />
+                <ProgramCard program={<LetterAnalyzer />} programTitle={'Letter Analzyer'} programSubtitle={'Enter some text and get a breakdown of how many of each letter was used'} />
+                <ProgramCard program={<TicTacToe />} programTitle={'TicTacToe'} programSubtitle={'The Classic Game of TicTacToe!'} />
             </Container>
         );
     }
