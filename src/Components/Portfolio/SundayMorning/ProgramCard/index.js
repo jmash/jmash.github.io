1c54c9990010462d14d9cd5a1f70ab6554397282
Jared M Ashcraft <jaredmashcraft@gmail.com>
2021-07-25 15:58:12 -0400

    :recycle: Refact SM morning comps into ProgramCard

--- src/Components/Portfolio/SundayMorning/ProgramCard/index.js
+++ src/Components/Portfolio/SundayMorning/ProgramCard/index.js
@@ -1,33 +1,19 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import ShowCodeButton from '../ShowCodeButton';
-import palindromeStyles from './ProgramCard.module.css';
-import cx from 'classnames';
 
-export default class Palindrome extends Component {
+export default class ProgramCard extends Component {
     render() {
         return (
-            <Card className={cx(palindromeStyles['topRightor'])}>
-                <Card.Title className={cx(palindromeStyles['palindromeStyles'])}>
-                <div>
-                    Palindrome Checker
-                </div>
-                <ShowCodeButton onClick={this.handleShowCodeButtonClick} position='side' active={this.state.palShowCodeActive} />
+            <Card className={'w-75'} >
+                <Card.Title>
+                    <div>
+                        {this.props.programTitle}
+                    </div>
                 </Card.Title>
                 
-                <Card.Subtitle>Checks if a word or sentence is a palindrome!</Card.Subtitle>
-                <Card.Body>
-                    <Form.Group>
-                        <Form.Label>Enter Word or Sentence to Check</Form.Label>
-                        <Form.Control type="text" placeholder="Word/Sentence" onChange={this.handlePalInputChange}></Form.Control>
-                        { this.state.palCurrent.matches('execAllowed') && <Form.Text>Only letters or numbers allowed (no special characters or punctuation)</Form.Text>}
-                        { this.state.palCurrent.matches('execDisallowed') && <Form.Text className={cx(palindromeStyles['input-error'])}>Only letters or numbers allowed (no special characters or punctuation) &gt;_&lt;</Form.Text>}
-                        <Button disabled={this.state.palCurrent.matches('execDisallowed')} onClick={() => palindromeSubj.next() }>Check for Palindromicity</Button>
-                        <Form.Control className={cx(palindromeStyles['no-resize'])} readOnly value={this.state.palDisplay} as="textarea" rows={1} />
-                    </Form.Group>
-                </Card.Body>
+                <Card.Subtitle>{this.props.programSubtitle}</Card.Subtitle>
+                {this.props.program}
+                
             </Card>
             
         );
