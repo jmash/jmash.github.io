5b87afa6cf54d776e47636165cca497f8fe3283c
Jared M Ashcraft <jaredmashcraft@gmail.com>
2021-07-25 17:31:51 -0400

    :art: prettying up the code card

--- src/Components/Portfolio/SundayMorning/ProgramCard/index.js
+++ src/Components/Portfolio/SundayMorning/ProgramCard/index.js
@@ -4,14 +4,12 @@ import Card from 'react-bootstrap/Card';
 export default class ProgramCard extends Component {
     render() {
         return (
-            <Card className={'w-75'} >
-                <Card.Title>
-                    <div>
+            <Card className={'w-75 my-3'} >
+                <Card.Title className={'pl-2 pt-1'}>
                         {this.props.programTitle}
-                    </div>
                 </Card.Title>
                 
-                <Card.Subtitle>{this.props.programSubtitle}</Card.Subtitle>
+                <Card.Subtitle className={'pl-3'}>{this.props.programSubtitle}</Card.Subtitle>
                 {this.props.program}
                 
             </Card>
