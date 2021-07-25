2fe6a226809bb53e0a145d11dcf67cbf346b5690
Jared M Ashcraft <jaredmashcraft@gmail.com>
2021-07-25 15:53:10 -0400

    :recycle: Refactor Palindrome component

--- src/Components/Portfolio/SundayMorning/Palindrome/index.js
+++ src/Components/Portfolio/SundayMorning/Palindrome/index.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import ShowCodeButton from '../ShowCodeButton';
-import ShowCodePanel from '../ShowCodePanel';
 import palindromeStyles from './Palindrome.module.css';
 import cx from 'classnames';
 import { interpret } from 'xstate';
@@ -33,7 +29,6 @@ export default class Palindrome extends Component {
             this.setState({ palCurrent })
         );
 
-        this.palRef = React.createRef();
 
         this.state = {
             palInput: "",
@@ -41,18 +36,11 @@ export default class Palindrome extends Component {
             palCurrent: palindromeMachine.initialState,
             palDisplayHeight: 0,
             palDisplayWidth: 0,
-            palShowCodeActive: false,
         }
     }
 
     componentDidMount() {
         this.palindromeService.start();
-
-
-        this.setState({
-            palDisplayHeight: this.palRef.current.clientHeight,
-            palDisplayWidth: this.palRef.current.clientWidth
-        });
 
         palindromeSubj.subscribe({
             next: () => {
@@ -76,7 +64,7 @@ export default class Palindrome extends Component {
         });
     }
 
-    handleShowCodeButtonClick = (e) => {
+    handleShowCodeButtonClick = () => {
         this.setState(prevState => ({
             palShowCodeActive: !prevState.palShowCodeActive
         }));
@@ -84,36 +72,16 @@ export default class Palindrome extends Component {
 
     render() {
         return (
-            <Row ref={this.palRef} className={cx(palindromeStyles['rowSpacing'])}>
-                <Col className={cx(palindromeStyles['paddingor'])}>
-                    <Card className={cx(palindromeStyles['topRightor'])}>
-                        <Card.Title className={cx(palindromeStyles['palindromeStyles'])}>
-                        <div>
-                            Palindrome Checker
-                        </div>
-                        <ShowCodeButton onClick={this.handleShowCodeButtonClick} position='side' active={this.state.palShowCodeActive} />
-                        </Card.Title>
-                        
-                        <Card.Subtitle>Checks if a word or sentence is a palindrome!</Card.Subtitle>
-                        <Card.Body>
-                            <Form.Group>
-                                <Form.Label>Enter Word or Sentence to Check</Form.Label>
-                                <Form.Control type="text" placeholder="Word/Sentence" onChange={this.handlePalInputChange}></Form.Control>
-                                { this.state.palCurrent.matches('execAllowed') && <Form.Text>Only letters or numbers allowed (no special characters or punctuation)</Form.Text>}
-                                { this.state.palCurrent.matches('execDisallowed') && <Form.Text className={cx(palindromeStyles['input-error'])}>Only letters or numbers allowed (no special characters or punctuation) &gt;_&lt;</Form.Text>}
-                                <Button disabled={this.state.palCurrent.matches('execDisallowed')} onClick={() => palindromeSubj.next() }>Check for Palindromicity</Button>
-                                <Form.Control className={cx(palindromeStyles['no-resize'])} readOnly value={this.state.palDisplay} as="textarea" rows={1} />
-                            </Form.Group>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <ShowCodePanel className={cx(palindromeStyles['paddingor'])}
-                    showComp="Palindrome" 
-                    panelHeight={ this.state.palDisplayHeight }
-                    panelWidth={ this.state.palDisplayWidth }
-                    active={ this.state.palShowCodeActive }
-                />
-            </Row>
+            <Card.Body>
+                <Form.Group>
+                    <Form.Label>Enter Word or Sentence to Check</Form.Label>
+                    <Form.Control type="text" placeholder="Word/Sentence" onChange={this.handlePalInputChange}></Form.Control>
+                    { this.state.palCurrent.matches('execAllowed') && <Form.Text>Only letters or numbers allowed (no special characters or punctuation)</Form.Text>}
+                    { this.state.palCurrent.matches('execDisallowed') && <Form.Text className={cx(palindromeStyles['input-error'])}>Only letters or numbers allowed (no special characters or punctuation) &gt;_&lt;</Form.Text>}
+                    <Button disabled={this.state.palCurrent.matches('execDisallowed')} onClick={() => palindromeSubj.next() }>Check for Palindromicity</Button>
+                    <Form.Control className={cx(palindromeStyles['no-resize'])} readOnly value={this.state.palDisplay} as="textarea" rows={1} />
+                </Form.Group>
+            </Card.Body>
         );
     }
 }
