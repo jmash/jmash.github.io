c80a75ab39bde89581fb9f4dbc0ff3ff042e3c3b
Jared M Ashcraft <jaredmashcraft@gmail.com>
2021-08-08 12:29:06 -0400

    :construction: makes card flip animation work

--- src/Components/Portfolio/SundayMorning/ProgramCard/index.js
+++ src/Components/Portfolio/SundayMorning/ProgramCard/index.js
@@ -3,12 +3,15 @@ import Row from 'react-bootstrap/Row';
 import ShowCodeCard from '../ShowCodeCard';
 import Card from 'react-bootstrap/Card';
 import ShowCodeButton from '../ShowCodeButton';
+import cx from 'classnames';
+import programCardStyles from './ProgramCard.module.css';
 
 export default class ProgramCard extends Component {
     constructor() {
         super();
 
         this.pcRef = React.createRef();
+        this.scRef = React.createRef();
 
         this.state = {
             pcDisplayWidth: 0,
@@ -24,10 +27,10 @@ export default class ProgramCard extends Component {
 
     render() {
         return (
-            <Row>
+            <Row style={{ transformStyle:'preserve-3d', perspective: 1100, minHeight: this.state.pcDisplayHeight }} className={cx(programCardStyles['containerCard'], 'mb-3 position-relative') }>
                 <Card style={{zIndex: 5}} ref={this.pcRef} className={'w-75 shadow-sm position-absolute'} >
-                    <div className={'position-relative'}>
-                        <ShowCodeButton />
+                    <div>
+                        <ShowCodeButton onClick={ this.props.clickFront } />
                         <Card.Title className={'pl-2 pt-1'}>
                             {this.props.programTitle}
                         </Card.Title>
@@ -38,7 +41,7 @@ export default class ProgramCard extends Component {
                         {this.state.pcDisplayHeight}
                     </div>
                 </Card>
-                <ShowCodeCard displayHeight={this.state.pcDisplayHeight} />
+                <ShowCodeCard onClick={this.props.clickBack} ref={ this.scRef } displayHeight={this.state.pcDisplayHeight} />
             </Row>
         );
     }
