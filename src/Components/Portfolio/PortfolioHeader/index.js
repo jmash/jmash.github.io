8ac14a692af6d9114efc4d86fb5cd98e9b1c6ed8
Jared M Ashcraft <jaredmashcraft@gmail.com>
2021-08-14 22:15:13 -0400

    :construction: divs on links to stop anim glitch

--- src/Components/Portfolio/PortfolioHeader/index.js
+++ src/Components/Portfolio/PortfolioHeader/index.js
@@ -8,15 +8,21 @@ import portfolioHeaderStyles from './PortfolioHeader.module.css';
 const PortfolioHeader = () => {
     return (
         <Navbar className={cx('nav-pills')}>
-            <LinkContainer activeClassName={cx(portfolioHeaderStyles['activePortfolioNavLink'])} className={cx('px-2', portfolioHeaderStyles['portfolioHeaderNavItem'])} to="/portfolio/sundaymorning">
-                <Nav.Item>Sunday Morning</Nav.Item>
-            </LinkContainer>
-            <LinkContainer activeClassName={cx(portfolioHeaderStyles['activePortfolioNavLink'])} className={cx('px-2', portfolioHeaderStyles['portfolioHeaderNavItem'])} to="/portfolio/vierbindungen">
-                <Nav.Item>Vierbindungen</Nav.Item>
-            </LinkContainer>
-            <LinkContainer activeClassName={cx(portfolioHeaderStyles['activePortfolioNavLink'])} className={cx('px-2', portfolioHeaderStyles['portfolioHeaderNavItem'])} to="/portfolio/clock">
-                <Nav.Item>Clock</Nav.Item>
-            </LinkContainer>
+            <div>
+                <LinkContainer activeClassName={cx(portfolioHeaderStyles['activePortfolioNavLink'])} className={cx('px-2', portfolioHeaderStyles['portfolioHeaderNavItem'])} to="/portfolio/sundaymorning">
+                    <Nav.Item>Sunday Morning</Nav.Item>
+                </LinkContainer>
+            </div>
+            <div>
+                <LinkContainer activeClassName={cx(portfolioHeaderStyles['activePortfolioNavLink'])} className={cx('px-2', portfolioHeaderStyles['portfolioHeaderNavItem'])} to="/portfolio/vierbindungen">
+                    <Nav.Item>Vierbindungen</Nav.Item>
+                </LinkContainer>
+            </div>
+            <div>
+                <LinkContainer activeClassName={cx(portfolioHeaderStyles['activePortfolioNavLink'])} className={cx('px-2', portfolioHeaderStyles['portfolioHeaderNavItem'])} to="/portfolio/clock">
+                    <Nav.Item>Clock</Nav.Item>
+                </LinkContainer>
+            </div>
         </Navbar>
     );
 };
