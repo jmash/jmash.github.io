287de1bcfc6116fcc02f8ef0bc654a42fe4ca209
Jared M Ashcraft <jaredmashcraft@gmail.com>
2021-08-28 16:50:51 -0400

    :art: remove useless nav import

--- src/Components/Portfolio/PortfolioHeader/index.js
+++ src/Components/Portfolio/PortfolioHeader/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { LinkContainer } from 'react-router-bootstrap';
 import cx from 'classnames';
