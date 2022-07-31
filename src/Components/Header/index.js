beb427446014d6ebfd91866c486aa20c9426a61d
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-07-31 16:47:52 -0400

    :construction: redoing navbar

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -17,17 +17,6 @@ const Header = () => {
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className={cx('mr-auto', 'px-md-4', 'flex-row', 'justify-content-center')}>
-                    <Nav.Link title="Link to Jared Ashcraft's Twitter Feed" href="https://twitter.com/jaredmashcraft">
-                            <FontAwesomeIcon className={ cx('mx-2', faStyles['fa-2x']) } icon={ faTwitter } />
-                    </Nav.Link>
-                    <Nav.Link title="Link to Jared Ashcraft's GitHub Repository" href="https://github.com/jmash">
-                            <FontAwesomeIcon className={ cx('mx-2', faStyles['fa-2x']) } icon={ faGithub } />      
-                    </Nav.Link>
-                    <Nav.Link title="Link to Jared Ashcraft's LinkedIn Profile" href="https://www.linkedin.com/in/jared-ashcraft-2985b939/">
-                            <FontAwesomeIcon className={ cx('mx-2', faStyles['fa-2x']) } icon={ faLinkedin } />
-                    </Nav.Link>
-                </Nav>
                 <Nav className={cx('ml-auto')}>
                     <Nav.Link className={cx('text-center')}>
                         <LinkContainer to="/about">
@@ -43,6 +32,17 @@ const Header = () => {
                         <LinkContainer to="/portfolio/sundaymorning">
                             <span>Portfolio</span>
                         </LinkContainer>  
+                    </Nav.Link>
+                </Nav>
+                <Nav className={cx('mr-auto', 'px-md-4', 'flex-row', 'justify-content-center')}>
+                    <Nav.Link title="Link to Jared Ashcraft's Twitter Feed" href="https://twitter.com/jaredmashcraft">
+                            <FontAwesomeIcon className={ cx('mx-2', faStyles['fa-2x']) } icon={ faTwitter } />
+                    </Nav.Link>
+                    <Nav.Link title="Link to Jared Ashcraft's GitHub Repository" href="https://github.com/jmash">
+                            <FontAwesomeIcon className={ cx('mx-2', faStyles['fa-2x']) } icon={ faGithub } />      
+                    </Nav.Link>
+                    <Nav.Link title="Link to Jared Ashcraft's LinkedIn Profile" href="https://www.linkedin.com/in/jared-ashcraft-2985b939/">
+                            <FontAwesomeIcon className={ cx('mx-2', faStyles['fa-2x']) } icon={ faLinkedin } />
                     </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
